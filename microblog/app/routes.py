afb14e707c165bb75722bb7962d0eb753ec8e8c6
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-01-06 16:54:49 +0800

    Finished Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World"
+    user = {'username':'Daniel'}
+
+    posts = [
+        {
+            'author': {'username': 'Shin'},
+            'body': 'It is a lovely day to travel to Singapore'
+        },
+        {
+            'author': {'username':'Ji Lin'},
+            'body': 'There was a tiger sighting in the forest once.'
+        }
+    ]
+    return render_template('index.html', title="Home Page", user = user, posts=posts)
+
