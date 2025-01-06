f6f8fd770e60f2cdf7bfbcbd2569da0a67d95031
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-01-06 17:19:32 +0800

    Finished config. Will move on to forms

---
+++ microblog/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config(object):
+    """
+    # TODO: When deployed on server, hardcoded SECRET_KEY value becomes unique and difficult
+    Should only be known to trusted maintainers of the site
+    #
+    """
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-in-a-million-years'
+    # Proects against CSRF. Generates secure Jtokens and signatures.
