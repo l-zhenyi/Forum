d44bc9e4a27774de332976f4d65cac495bb0fffc
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-01-07 21:39:32 +0800

    Fixed flask extensions by editing venv and interpreter path

--- microblog/config.py
+++ microblog/config.py
@@ -1,10 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    """
-    # TODO: When deployed on server, hardcoded SECRET_KEY value becomes unique and difficult
-    Should only be known to trusted maintainers of the site
-    #
-    """
+    #TODO: When deployed on server, hardcoded SECRET_KEY value becomes unique and difficult
+    #Should only be known to trusted maintainers of the site
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-in-a-million-years'
     # Proects against CSRF. Generates secure tokens and signatures.
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
