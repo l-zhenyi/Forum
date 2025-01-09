80cd79bfd5840290e3676cbac262c6e2c95c7d9f
l-zhenyi <zhenyi.lim.mail@gmail.com>
2025-01-09 14:54:52 +0800

    Added registration html page and route

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse 
 from app.models import User
@@ -46,17 +46,16 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-# Clearing database for next session
-# (venv) $ flask db downgrade base
-# (venv) $ flask db upgrade
-
-# Cleaning flask shell
-#  >>> users = User.query.all() 
-# >>> for u in users: 
-# ...     db.session.delete(u) 
-# ... 
-# >>> posts = Post.query.all() 
-# >>> for p in posts: 
-# ...     db.session.delete(p) 
-# ... 
-# >>> db.session.commit() 
+@app.route('/register', methods=['GET', 'POST']) 
+def register(): 
+    if current_user.is_authenticated: 
+        return redirect(url_for('index')) 
+    form = RegistrationForm() 
+    if form.validate_on_submit(): 
+        user = User(username=form.username.data, email=form.email.data) 
+        user.set_password(form.password.data) 
+        db.session.add(user) 
+        db.session.commit() 
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login')) 
+    return render_template('register.html', title='Register', form=form) 
